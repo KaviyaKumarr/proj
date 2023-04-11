8d212fd9df0342991f281e6e56c57e5ae105c942
KaviyaKumarr <130345024+KaviyaKumarr@users.noreply.github.com>
2023-04-11 11:31:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-window.addEventListener('load', async () => {
-  if (window.ethereum) {
-    window.web3 = new Web3(window.ethereum);
-    try {
-      // Request account access if needed
-      await window.ethereum.enable();
-      // Acccounts now exposed
-      web3.eth.sendTransaction({/* ... */});
-    } catch (error) {
-      // User denied account access...
-    }
-  }
-  // Legacy dapp browsers...
-  else if (window.web3) {
-    window.web3 = new Web3(web3.currentProvider);
-    // Acccounts always exposed
-    web3.eth.sendTransaction({/* ... */});
-  }
-  // Non-dapp browsers...
-  else {
-    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-  }
-});
-
 const connectButton = document.getElementById('connectButton');
 
+
 connectButton.addEventListener('click', async () => {
-  // Will open the MetaMask UI
-  // You should disable this button while the request is pending!
-  await ethereum.request({ method: 'eth_requestAccounts' });
+ 
+  if (window.ethereum) {
+    try {
+      
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      console.log('Connected to Metamask');
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.error('Metamask not installed');
+  }
 });
